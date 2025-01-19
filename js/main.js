3eb9763ef5f11a938e01220b81e0ce640eb95b79
Ibrohim Umarov <ibrohimumarov@MacBook-Pro-Ibrohim.local>
2025-01-19 20:16:59 +0500

    hero, description and service sections done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 let elMenuBtn = document.querySelector(".site-header__hidden-menu-btn");
-let elMenuBtnImg = document.querySelector(".site-header__hidden-btn-img");
+let elMenuBtnImg = document.querySelector(".site-header__hidden-btn-img");  
 let elMobileNavList = document.querySelector(".site-header__hidden-nav");
 let elMobileNavListShow = document.querySelector(".site-header__hidden-show");
-let elMobileNavListRemoveBtn = document.querySelector(".site-header__menu-remove-btn");
-let elMobileNavListRemove = document.querySelector(".site-header__hide")
+// let elMobileNavListRemoveBtn = document.querySelector(".site-header__menu-remove-btn");
+// let elMobileNavListRemove = document.querySelector(".site-header__hide")
 
 elMenuBtnImg.addEventListener("click", function(){
-    elMobileNavList.classList.add("site-header__hidden-show")
+    elMobileNavList.classList.toggle("site-header__hidden-show");
 })
-elMobileNavListRemoveBtn.addEventListener("click" , function(){
-    elMobileNavList.classList.add("site-header__hide")
-})
+// elMobileNavListRemoveBtn.addEventListener("click" , function(){
+//     elMobileNavList.classList.toggle("site-header__hide")
+// })
+
